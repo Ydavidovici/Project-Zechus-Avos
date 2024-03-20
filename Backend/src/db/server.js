1ef1be80b6f502d0729a4c30fdad4cd14adc3df0
Ydavidovici <150648286+Ydavidovici@users.noreply.github.com>
2024-03-20 14:26:23 -0400

    restructured sponsorship options

--- Backend/src/db/server.js
+++ Backend/src/db/server.js
@@ -101,7 +101,7 @@ app.post('/create-payment-intent', async (req, res) => {
 });
 
 // Listen to the App Engine-specified port, or 3000 otherwise
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3100;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 
 // Import the database initialization script
 require('./db/server.js');
