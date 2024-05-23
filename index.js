898a6ae7d7a82d8658ba778582cdbd30fc7a6f0d
Ydavidovici <150648286+Ydavidovici@users.noreply.github.com>
2024-05-23 19:46:02 -0400

    enhanced 500 logging

--- index.js
+++ index.js
@@ -304,7 +304,8 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    return res.status(500).send('Something broke!');
+    console.error('Error:', err.stack);
+    res.status(500).json({ message: 'Something broke!', error: err.message });
 });
 
 app.listen(PORT, () => {
